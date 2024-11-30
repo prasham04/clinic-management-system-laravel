cb21f5b5f3692bae17aec1471d51fbf3f1cd5b01
Prasham Mehta <prashammehta04@gmail.com>
2024-12-01 00:11:58 +0530

    clinic management system

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -15,7 +15,7 @@
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Uses</h3>
+                <h3 class="card-title">Users</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
